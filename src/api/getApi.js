f0f53205b333e1b6fc558bc6b514cc1bc69b8767
sz9517 <sylvia9517@gmail.com>
2023-08-11 13:27:42 +0800

    08/11

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -4,14 +4,15 @@ import { api, chatApi } from "./api";
 export const getHistoryFromAgent = ({ user_id } = {}) => {
   return chatApi.get("", {
     params: {
-      ouath:
-        process.env.REACT_APP_CHAT_TYPE === "agent"
-          ? "64be0a7d9811b"
-          : "64ba3ae689985",
+      // ouath:
+      //   process.env.REACT_APP_CHAT_TYPE === "agent"
+      //     ? "64be0a7d9811b"
+      //     : "64ba3ae689985",
       ouath: Cookies.get("ouath"),
-      csId: process.env.REACT_APP_CHAT_TYPE === "agent" && Cookies.get("csId"),
-      csId:Cookies.get("csId") || "1",
-      user_id: process.env.REACT_APP_CHAT_TYPE === "agent" && user_id || 1,
+      csId: process.env.REACT_APP_CHAT_TYPE === "agent" && Cookies.get("csId") || "1",
+      // csId: Cookies.get("csId") || "1",
+      // csId:1,
+      user_id: process.env.REACT_APP_CHAT_TYPE === "agent" && user_id || "1",
     },
   });
 };
@@ -19,10 +20,10 @@ export const getHistoryFromAgent = ({ user_id } = {}) => {
 export const getMemberList = ({ mem_id } = {}) => {
   return chatApi.get("/member", {
     params: {
-      ouath:
-        process.env.REACT_APP_CHAT_TYPE === "agent"
-          ? "64be0a7d9811b"
-          : "64ba3ae689985",
+      // ouath:
+      //   process.env.REACT_APP_CHAT_TYPE === "agent"
+      //     ? "64be0a7d9811b"
+      //     : "64ba3ae689985",
       ouath: Cookies.get("ouath"),
       mem_id: mem_id,
     },
--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,12 +1,15 @@
 import { api, chatApi, chatApiWithoutHeader } from "./api";
+import Cookies from "js-cookie";
 
 export const actionBackSendMessage = ({ user_id, message, image } = {}) => {
   return chatApi.post("", {
-    ouath:
-      process.env.REACT_APP_CHAT_TYPE === "agent"
-        ? "64be0a7d9811b"
-        : "64ba3ae689985",
-    csId: process.env.REACT_APP_CHAT_TYPE === "agent" && "1",
+    // ouath:
+    //   process.env.REACT_APP_CHAT_TYPE === "agent"
+    //     ? "64be0a7d9811b"
+    //     : "64ba3ae689985",
+    // csId: process.env.REACT_APP_CHAT_TYPE === "agent" && "1",
+    ouath: Cookies.get("ouath"),
+    csId: process.env.REACT_APP_CHAT_TYPE === "agent" && Cookies.get("csId") || "1",
     message: message,
     image: image,
     user_id: process.env.REACT_APP_CHAT_TYPE === "agent" && user_id,
